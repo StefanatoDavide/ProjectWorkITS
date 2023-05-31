6dcbb4a5348adda0d85a75e461c85b14e449dc24
Luca-Minello <luca.minello@allievi.itsdigitalacademy.com>
2023-05-31 10:20:17 +0200

    Aggiunta Header

--- Ricerche/RicercaMovimenti1.php
+++ Ricerche/RicercaMovimenti1.php
@@ -4,9 +4,153 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ricerca</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Login</title>
+    
+    <script>
+        function isOnlyDigits(string) {
+            for (let i = 0; i < string.length; i++) {
+                var ascii = string.charCodeAt(i);
+                if (ascii < 48 || ascii > 57) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        function CercaUltimi() {
+            ricercaString = FormRicercaUltimi.intRicerca.value;
+            if (isOnlyDigits(ricercaString)==FALSE) {
+                return;
+            }
+            if (ricercaString.include("-")) {
+                return;
+            }
+            FormRicercaUltimi.submit();
+        }
+        
+        function CercaData(){
+            daString = FormRicercaData.IDda.value;
+            aString = FormRicercaData.IDa.value;
+            FormRicercaData.submit();
+        }
+    </script>
 </head>
 <body>
+    <header>
+        <nav class="navbar navbar-expand-md bg-light navbar-light">
+            <a class="navbar-brand" href="http://localhost/Projectworkits/index.php" colour>
+                <img border="0" alt="W3Schools" src="http://localhost/Projectworkits/CoinLogo.jpg" width="50" height="50">
+            </a>  
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                <ul class="navbar-nav">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                            Account
+                        </a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="http://localhost/Projectworkits/index.php">Informazioni account</a>
+                            <a class="dropdown-item" href="http://localhost/Projectworkits/Account/ModificaPassword.php">Modifica password</a>
+                            <a class="dropdown-item text-danger" href="http://localhost/Projectworkits/Account/LogOut.php">Log Out</a>
+                        </div>
+                    </li> 
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                            Ricerca ultimi movimenti
+                        </a>
+                        <div class="dropdown-menu">
+
+                            <form class= "form-inline" name= "FormRicercaUltimi" action="" method="get">
+                                <input class="form-control" type="number" id ="intRicerca" name="IntUltimi" placeholder="Trova ultimi X movimenti">
+                                <button class="btn btn-success btn-block " type="submit" onclick="CercaUltimi()">Cerca</button>
+                            </form>
+                            <?php
+                                if(isset($_GET['IntUltimi'])){
+                                    $intRicercaUltimi = $_GET['IntUltimi'];
+                                    $url = "http://localhost/Projectworkits/Ricerche/RicercaMovimenti1.php?ID=".$intRicercaUltimi;
+                                    
+                                    if (empty($intRicercaUltimi) ==false){
+                                        header("Location: ".$url);
+                                    }
+                                }
+                            ?>
+
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                            Ricerca per tipologia movimenti
+                        </a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="http://localhost/Projectworkits/Ricerche/RicercaMovimenti2.php?ID=2">Bonifico Entrata</a>
+                            <a class="dropdown-item" href="http://localhost/Projectworkits/Ricerche/RicercaMovimenti2.php?ID=3">Versamento Bancomat</a>	
+                            <a class="dropdown-item" href="http://localhost/Projectworkits/Ricerche/RicercaMovimenti2.php?ID=4">Bonifico Uscita</a>
+                            <a class="dropdown-item" href="http://localhost/Projectworkits/Ricerche/RicercaMovimenti2.php?ID=5">Prelievo Contanti</a>
+                            <a class="dropdown-item" href="http://localhost/Projectworkits/Ricerche/RicercaMovimenti2.php?ID=6">Pagamento Utenze</a>
+                            <a class="dropdown-item" href="http://localhost/Projectworkits/Ricerche/RicercaMovimenti2.php?ID=7">Ricarica Telefonica</a>	
+                            <a class="dropdown-item" href="http://localhost/Projectworkits/Ricerche/RicercaMovimenti2.php?ID=8">Pagamento Bollette</a>	
+                            <a class="dropdown-item" href="http://localhost/Projectworkits/Ricerche/RicercaMovimenti2.php?ID=9">Pagamento F24</a>
+                            <a class="dropdown-item" href="http://localhost/Projectworkits/Ricerche/RicercaMovimenti2.php?ID=10">Bollettino Postale</a>	
+                            <a class="dropdown-item" href="http://localhost/Projectworkits/Ricerche/RicercaMovimenti2.php?ID=11">Ricarica Carta Prepagata</a>
+                            <a class="dropdown-item" href="http://localhost/Projectworkits/Ricerche/RicercaMovimenti2.php?ID=12">Bollo Auto</a>
+                            <a class="dropdown-item" href="http://localhost/Projectworkits/Ricerche/RicercaMovimenti2.php?ID=13">Accredito Stipendio</a>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                            Ricerca per data movimento
+                        </a>
+                        <div class="dropdown-menu">
+                            <form class="form-inline needs-validation" name= "FormRicercaData" method="get" action="http://localhost/Projectworkits/Ricerche/RicercaMovimenti3.php">
+                                <div class="form-group mx-auto ">
+                                    <label for="da" class="mr-sm-2">Da:  </label>
+                                    <input class="form-control  " type="date" id = "IDda" name="Datada">
+                                </div> </br>
+                                <div class="form-group mx-auto ">
+                                    <label for="a" class="mr-sm-2"> A:</label>
+                                    <input class="form-control " type="date" id = "IDa" name="DataA">
+                                </div>
+                                <button class="btn btn-success btn-block " type="submit" >Cerca</button>
+
+                                <?php
+                                
+                                if(isset($_GET['Datada']) && isset($_GET["DataA"])){
+                                    
+                                    $data1 = $_GET['Datada'];
+                                    $data2 = $_GET['DataA'];
+                                    $url = "http://localhost/Projectworkits/Ricerche/RicercaMovimenti3.php?DA=".$data1."&A=".$data2;
+                                    
+                                    
+                                    if (empty($data1) ==false){
+                                        header("Location: http://localhost/Projectworkits/Ricerche/RicercaMovimenti3.php");
+                                    }else{
+                                        
+                                    }                                        
+                                }
+                                ?>
+                            </form>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                            Servizi
+                        </a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">Ricarica Telefonica</a>
+                            <a class="dropdown-item" href="#">Bonifico</a>
+                        </div>
+                    </li> 
+                </ul>
+            </div>
+        </nav>
+    </header>
 <?php
 // session_start();
 // $userID=0;
@@ -38,8 +182,8 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 $numeroMovimenti=0;
 // Ottieni il numero di movimenti da visualizzare
-if($_GET["NumeroMovimenti"]!=""){
-    $numeroMovimenti = intval($_GET["NumeroMovimenti"]);
+if($_GET["ID"]!=""){
+    $numeroMovimenti = intval($_GET["ID"]);
     $query = "SELECT m.Data, m.Importo, c.NomeCategoria
           FROM tmovimenticontocorrente m
           INNER JOIN tcategoriemovimenti c ON m.CategoriaMovimentoID = c.CategoriaMovimentoID
@@ -85,7 +229,7 @@ $conn->close();
 <body>
     <h1>Ricerca Movimenti</h1>
 
-    <form method="post" action="http://localhost/Projectworkits/Ricerche/RicercaMovimenti1.php?NumeroMovimenti=">
+    <form method="post" action="http://localhost/Projectworkits/Ricerche/RicercaMovimenti1.php?ID=&NumeroMovimenti=">
         <label for="numero_movimenti">Numero Movimenti:</label>
         <input type="number" id="numero_movimenti" name="numero_movimenti" value="<?php echo $numeroMovimenti; ?>">
         <button type="submit">Cerca</button>
